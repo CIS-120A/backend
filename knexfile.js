249915abc1041c1e5c866b764718ab10cc88ada6
Joe-Thompson <bmo3696@gmail.com>
2021-04-28 19:22:49 -0400

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -33,7 +33,14 @@ module.exports = {
 
    production: {
     client: "pg",
-    connection: "postgres://tjjehsnhxzeduy:d0b93466a66826863788b59a3ee71d95734ee93c17712fada461ec9063dd164d@ec2-52-45-73-150.compute-1.amazonaws.com:5432/d35r3augl6inmn",
+    connection: {
+      host: "ec2-52-45-73-150.compute-1.amazonaws.com",
+      database: "d35r3augl6inmn",
+      user: "tjjehsnhxzeduy",
+      port: 5432,
+      password: "d0b93466a66826863788b59a3ee71d95734ee93c17712fada461ec9063dd164d",
+    },
+     useNullAsDefault: true,
     migrations: {
       directory: "./db/migrations",
     },
